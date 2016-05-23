a5054958ec59027e4022be038dbe8d015118829b
vitpospisil <vit.pospisil@pslib.cz>
2016-05-23 15:54:43 +0200

    initial commit

---
+++ HealthCheck/HealthCheck/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+
+
+namespace HealthCheck
+{
+    class HealthClass
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
