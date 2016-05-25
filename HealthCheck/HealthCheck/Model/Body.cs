e3b4b786a7a96fe15f7f6caff9ad769576f3d014
vitpospisil <vit.pospisil@pslib.cz>
2016-05-25 10:04:12 +0200

    Iam not master

--- HealthCheck/HealthCheck/Model/Body.cs
+++ HealthCheck/HealthCheck/Model/Body.cs
@@ -14,88 +14,34 @@ namespace HealthCheck.Model
     class Body
     {
         List<Worker> Workers = new List<Worker>();
-        public string Output { get; private set; } = "";
+        public StringWriter Output = new StringWriter();
 
         public Body()
         {
-            DoThings();
-            
-        }
-
-        public void DoThings()
-        {
+            Output.Write("Status - ");
             //get certificate
             ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
             //ask for http status
             HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create("https://10.0.1.221:9000/");
             webRequest.AllowAutoRedirect = false;
             HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse();
-            Output += response.StatusCode.ToString();
-            if(Output != "OK") { Console.WriteLine(Output); return; }
-            //download file (as .txt)
+            Output.Write(response.StatusCode.ToString());
+            if(Output. != "Status - OK") { Console.WriteLine(Output); return; }
+            //download file (as string)
             WebClient webClient = new WebClient();
-            webClient.DownloadFile("https://10.0.1.221:9000/","workers.txt");
-            //read .txt file and convert it to string
-            string xml = File.ReadAllText(Environment.CurrentDirectory + "\\workers.txt");
+            string text = webClient.DownloadString("https://10.0.1.221:9000/");
+            //Prepare for output
+            JObject stuff = JObject.Parse(text);
+            JArray workers = (JArray)stuff["Workers"];
 
-            dynamic stuff = JObject.Parse(xml);
-            Output += Environment.NewLine + "Is Db Connected - " + stuff.IsDbConnected + Environment.NewLine
-                + " Workers - " + stuff.Workers.Name;
+            Console.WriteLine(workers[0]["Name"]);
+
+            Output += Environment.NewLine + "Is Db Connected - " + stuff["IsDbConnected"] + Environment.NewLine +
+                      "Workers count - " + workers.Count + Environment.NewLine;
+                      
             Console.WriteLine(Output);
             Console.ReadKey();
 
-
-
-
-
-
-
-
-            //Need download or copy the XML file from 10.0.1.221:9000
-            /*
-            using (XmlReader xr = XmlReader.Create(@"workers.xml"))
-            {
-                
-                string name = "";
-                string status = "";
-                string element = "";
-
-                while (xr.Read())
-                {
-                    if (xr.NodeType == XmlNodeType.Element)
-                    {
-                        element = xr.Name;
-                        
-                    }
-                    else if (xr.NodeType == XmlNodeType.Text)
-                    {
-                        switch (element)
-                        {
-                            case "IsDbConnected":
-                                Output +="IsDbConnected - " + xr.Value + Environment.NewLine;
-                                break;
-                            case "ServiceStatus":
-                                Output +="ServiceStatus - " + xr.Value + Environment.NewLine;
-                                break;
-                            case "Version":
-                                Output += "Version: " + xr.Value + Environment.NewLine;
-                                break;
-                            case "Name":
-                                name = xr.Value;
-                                break;
-                            case "Status":
-                                status = xr.Value;
-                                break;
-                        }
-                    }
-
-                    else if ((xr.NodeType == XmlNodeType.EndElement) && (xr.Name == "WorkerStatusDto"))
-                        Workers.Add(new Worker(name, status));
-                }
-                
-                WriteIt(Workers);
-            }
-            */
         }
 
         public void WriteIt(List<Worker> workers)
