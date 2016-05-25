1c3869849c618408ebe62d250ff717e288078ba4
vitpospisil <vit.pospisil@pslib.cz>
2016-05-25 14:58:54 +0200

    Day 2, preparing for 1 request

---
+++ HealthCheck/HealthCheck/Model/HealthCheckDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCheck.Model
+{
+    class HealthCheckDto
+    {
+        public bool IsDbConnected { get; set; }
+        public string ServiceStatus { get; set; }
+        public string Version { get; set; }
+        public List<WorkerStatusDto> Workers = new List<WorkerStatusDto>();
+    }
+}
