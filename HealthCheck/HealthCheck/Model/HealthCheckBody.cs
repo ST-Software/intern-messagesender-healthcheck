f431a1ba6d1f613b84c952a7485fd2f13d298e72
Antonín Drahovzal <antonin.drahovzal@pslib.cz>
2016-05-30 13:50:49 +0200

    Adress

---
+++ HealthCheck/HealthCheck/Model/HealthCheckBody.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Configuration;
+
+namespace HealthCheck.Model
+{
+    class HealthCheckBody
+    {
+        public static void Check(string text)
+        {
+            string adress = ConfigurationManager.AppSettings["adress"];
+            try
+            {
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(adress);
+                httpWebRequest.AllowAutoRedirect = false;
+                Stream stream = httpWebRequest.GetRequestStream();
+                stream.Close();
+
+                WebResponse response = httpWebRequest.GetResponse();
+                stream = response.GetResponseStream();
+
+                StreamReader streamreader = new StreamReader(stream);
+                text = streamreader.ReadToEnd();
+
+                streamreader.Close();
+                stream.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
--- HealthCheck/HealthCheck/Program.cs
+++ HealthCheck/HealthCheck/Program.cs
@@ -7,40 +7,19 @@ using System.Net;
 using System.IO;
 using System.Timers;
 using System.Configuration;
+using HealthCheck.Model;
 
 
 namespace HealthCheck
 {
     class Program
     {
-       
-
+      
         static void Main(string[] args)
         {
             ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
             string adress = ConfigurationManager.AppSettings["adress"];
-            string path = ConfigurationManager.AppSettings["filepPath"];
-            string text = "";
-            try
-            {
-                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(adress);
-                httpWebRequest.AllowAutoRedirect = false;
-                Stream stream = httpWebRequest.GetRequestStream();
-                stream.Close();
-
-                WebResponse response = httpWebRequest.GetResponse();
-                stream = response.GetResponseStream();
-
-                StreamReader streamreader = new StreamReader(stream);
-                text = streamreader.ReadToEnd();
-
-                streamreader.Close();
-                stream.Close();
-            }
-            catch 
-            {
-                Console.WriteLine("Error");
-            }
+            string path = ConfigurationManager.AppSettings["filePath"];
             Console.WriteLine(adress);
             Console.ReadKey();
         }
